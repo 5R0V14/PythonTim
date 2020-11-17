be63a1ee74c9438e8fdf97723d8eaf443ee819e3
5R0V14 <timke@bk.ru>
2020-11-17 17:48:36 +0200

    Added Lesson_01

---
+++ Lesson_1/Escape.py
@@ -0,0 +1,7 @@
+print('Escape sequences\n'
+      '\\a\t\tBell (alert)\n'
+      '\\b\t\tBackspace\n'
+      '\\n\t\tNew line\n'
+      '\\\t\tBackslash\n'
+      '\\"\t\tDouble qoutation mark\n'
+      "\\'\t\tSingle qoutation mark" )
