a09053dccb0b297cafdacbf2966c4d2b63136b76
5R0V14 <timke@bk.ru>
2020-11-19 20:33:15 +0200

    HW correction

--- Lesson_02/Number.py
+++ Lesson_02/Number.py
@@ -1,3 +1,8 @@
-a=input("Введите целое число: ")
-b=a[::-1]
-print("Обратное число вашему: ", b)
+number = int(input("Введите ваше число: "))
+
+num1 = number % 10
+num2 = number // 10 % 10
+num3 = number // 100
+
+result = num1 * 100 + num2 * 10 + num3
+print("Обратное ему число:", result)
